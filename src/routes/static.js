b2a64da11542039e6b29731fe8fe1de6048978f5
Soo Kim <chizbob@gmail.com>
2019-03-14 14:21:41 -0700

    scaffolding

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,4 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const staticController = require("../controllers/staticController")
 
+router.get("/", staticController.index)
 module.exports = router
